c267471565c5b472f6dc803221a47bcea842a263
Johnatan Torres <johnatantorres1995@gmail.com>
2022-05-22 09:12:09 -0500

    Se agregan nuevas clases llamadas driver y user

--- PHP/index.php
+++ PHP/index.php
@@ -3,8 +3,6 @@ require_once("car.php");
 require_once("uberX.php");
 require_once("account.php");
 require_once("uberPool.php");
-$uberX = new UberX("AWS543",new Account("Andrés Herrera","RET423"),"Chevrolet", "Spark");
+$uberX = new UberX("AWS543",new Account("Andrés Ortiz","RET423"),"Chevrolet", "Spark");
 $uberX->PrintDataCar();
-$uberPool = new UberPool("ATR543",new Account("Andrés Hernandez","RFJ423"),"Dodge", "Attitude");
-$uberPool->PrintDataCar();
 ?>
