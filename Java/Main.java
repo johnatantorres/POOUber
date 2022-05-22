c81f6a56068bd7323bc29528f59a687ad9570a9a
Johnatan Torres <johnatantorres1995@gmail.com>
2022-05-22 18:31:54 -0500

    Se agregan los setters y getters a la clase Car para una optima asignación de los valores de passenger

--- Java/Main.java
+++ Java/Main.java
@@ -3,12 +3,12 @@ package Java;
 class Main {
     public static void main(String[] args) {
         System.out.println("Hola mundo");
-        Car car = new Car("AMQ123", new Account("Andrés Herrera","AND123"));
-        car.passenger = 4;
-        car.printDataCar();
+        UberX uberX = new UberX("AMQ123", new Account("Andrés Herrera","AND123"),"Chevrolet","Sonic");
+        uberX.setPassenger(3);
+        uberX.printDataCar();
 
-        Car car2 = new Car("QWE643", new Account("Andrea Herrera", "ANDA874"));
+        /*Car car2 = new Car("QWE643", new Account("Andrea Herrera", "ANDA874"));
         car2.passenger = 3;
-        car2.printDataCar();
+        car2.printDataCar();*/
     }
 }
